9001ff903b5e16d49b3965e7b15017cb0f72d43e
aprashant664-sudo <aprashant664@gmail.com>
2026-04-23 08:58:43 +0545

    Create abstract Account class with basic methods

--- Account.java
+++ Account.java
@@ -1,1 +1,28 @@
+public abstract class Account {
 
+    protected int accountNumber;
+    protected int pin;
+    protected double balance;
+
+    public Account(int accountNumber, int pin, double balance) {
+        this.accountNumber = accountNumber;
+        this.pin = pin;
+        this.balance = balance;
+    }
+
+    public int getPin() {
+        return pin;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public boolean withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+            return true;
+        }
+        return false;
+    }
+}
