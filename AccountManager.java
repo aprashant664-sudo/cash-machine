d03564b78f40d66a0cd6d4b03e4637b20fbc839d
aprashant664-sudo <aprashant664@gmail.com>
2026-04-23 09:00:35 +0545

    Update AccountManager.java

--- AccountManager.java
+++ AccountManager.java
@@ -1,1 +1,26 @@
+import java.util.ArrayList;
 
+public class AccountManager {
+
+    private ArrayList<Account> accounts;
+
+    public AccountManager() {
+
+        accounts = new ArrayList<>();
+
+        accounts.add(new SavingsAccount(1001, 1234, 1000));
+        accounts.add(new CurrentAccount(1002, 1111, 1500));
+        accounts.add(new SavingsAccount(1003, 2222, 2000));
+        accounts.add(new CurrentAccount(1004, 3333, 500));
+    }
+
+    public Account validatePin(int pin) {
+
+        for (Account acc : accounts) {
+            if (acc.getPin() == pin) {
+                return acc;
+            }
+        }
+        return null;
+    }
+}
